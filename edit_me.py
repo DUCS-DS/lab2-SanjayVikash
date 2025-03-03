12fbbd0ed486da60d68c0bb5b891aaf26763451e
SanjayVikash <sanjayvikashvasidhar@gmail.com>
2025-03-03 12:14:18 -0600

    Added biconditional argument

--- edit_me.py
+++ edit_me.py
@@ -14,23 +14,26 @@ def monotonic(lst):
         if lst[i] < lst[i-1]:
             non_decreasing = False
         if lst[i] == lst[i-1]:
-            return True
+            pass
     
     if non_increasing == False and non_decreasing == False:
         non_decreasing = True
-        non_decreasing = True
-    return non_increasing and non_decreasing
+        non_increasing = True
+    return ((non_increasing and non_decreasing) or (not(non_increasing) and not(non_decreasing)))
             
 
+# False = monotonic, True = not monotonic 
 
-
-num = [1,2,3,4,2,6,7] #monotonic
+num = [1,2,3,4,5,6,7]# monotonic
 num1= [1] #not list 
-num2 = [1,2,3,4,4,5,6,7] #not monotonic
+num2 = [1,2,3,4,4,5,6,7] # monotonic
 num3 = [5,4,3,2,1] # monotonic
 num4 = [5,4,3,4,5] # not monotonic 
+num5 = [1,1,1,1,1,3,3,3,3,5,5,5,5,6,6,6,6]
 print(monotonic(num))
 print(monotonic(num1))
 print(monotonic(num2))
 print(monotonic(num3))
 print(monotonic(num4))
+print(monotonic(num5))
+
